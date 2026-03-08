c51543144526edf27c239bb8e2eeb9edef948291
JitendraSies <jitendrap.mca25@siescoms.sies.edu.in>
2026-03-08 19:58:44 +0530

    Update test1.java

--- test1.java
+++ test1.java
@@ -1,7 +1,7 @@
 public class test1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        int a = 15;
+        int a = 25;
         int b = 10;
         int add = a + b;
         int sub = a - b;
